30cd2b86ad5990da9ed37c20b1e6d0d29ee811be
StaticCloud <diegomarrs@gmail.com>
2021-12-24 15:35:53 -0800

    updated template

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -126,13 +128,12 @@ const promptProject = portfolioData => {
     });
 }
 
-promptUser().then(promptProject).then(portfolioData => console.log(portfolioData));
-/*const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        const pageHTML = generatePage(portfolioData);
 
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output!');
-})*/
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+        })
+    });
